42a1f6de1554e91f34f2912739e1c262ed24297e
s21165 <S21165@pjwstk.edu.pl>
2021-05-22 22:34:35 +0200

    50000 int's more to sort

--- sortowanie.py
+++ sortowanie.py
@@ -1,23 +1,7 @@
+import sys
+sys.setrecursionlimit(100000)
+from random import *
 from time import perf_counter_ns
-
-def qs(arr, p, r):
-    if p < r:
-        q = part(arr, p, r)
-        qs(arr, p, q-1)
-        qs(arr, q+1, r)
-
-def part(arr, p, r):
-    pivot = arr[r]
-    smaller = p
-    for j in range(p, r):
-        if arr[j] <= pivot:
-            arr[smaller], arr[j] = arr[j], arr[smaller]
-            smaller = smaller + 1
-    arr[smaller], arr[r] = arr[r], arr[smaller]
-    return smaller
-
-
-
 
 def Heapify(arr, n, i):
     najwieksza = i
@@ -31,8 +15,8 @@ def Heapify(arr, n, i):
         arr[i], arr[najwieksza] = arr[najwieksza], arr[i]
         Heapify(arr, n, najwieksza)
 def Build_Heap(arr, n):
-    pierwszawsza = n // 2 - 1;
-    for i in range(pierwszawsza, -1, -1):
+    lewowsza = n // 2 - 1;
+    for i in range(lewowsza, -1, -1):
         Heapify(arr, n, i);
 def Heap_Sort(arr):
     n = len(arr)
@@ -45,7 +29,7 @@ def Heap_Sort(arr):
 
 
 
-minrun = 32
+minrun = 32                 # po ile dzielimy na czesc, najszczęściej wybiera się 32/64
 def InsSort(arr, start, end):
     for i in range(start + 1, end + 1):
         elem = arr[i]
@@ -60,8 +44,8 @@ def InsSort(arr, start, end):
 def merge(arr, start, mid, end):
     if mid == end:
         return arr
-    pierwsza = arr[start:mid + 1]
-    ostatnia = arr[mid + 1:end + 1]
+    lewo = arr[start:mid + 1]
+    prawo = arr[mid + 1:end + 1]
     dlugosc1 = mid - start + 1
     dlugosc2 = end - mid
     ind1 = 0
@@ -69,21 +53,21 @@ def merge(arr, start, mid, end):
     ind = start
 
     while ind1 < dlugosc1 and ind2 < dlugosc2:
-        if pierwsza[ind1] < ostatnia[ind2]:
-            arr[ind] = pierwsza[ind1]
+        if lewo[ind1] < prawo[ind2]:
+            arr[ind] = lewo[ind1]
             ind1 += 1
         else:
-            arr[ind] = ostatnia[ind2]
+            arr[ind] = prawo[ind2]
             ind2 += 1
         ind += 1
 
     while ind1 < dlugosc1:
-        arr[ind] = pierwsza[ind1]
+        arr[ind] = lewo[ind1]
         ind1 += 1
         ind += 1
 
     while ind2 < dlugosc2:
-        arr[ind] = ostatnia[ind2]
+        arr[ind] = prawo[ind2]
         ind2 += 1
         ind += 1
 
@@ -108,75 +92,101 @@ def TimSort(arr):
 
 
 
-arr = [7,4,3,8,10,3,3,13,14,21,27]
+
+def partition(arr,low,high):
+    i = ( low-1 )
+    pivot = arr[high]
+    for j in range(low , high):
+        if   arr[j] <= pivot:
+            i = i+1
+            arr[i],arr[j] = arr[j],arr[i]
+    arr[i+1],arr[high] = arr[high],arr[i+1]
+    return i + 1
+
+
+def QuickSort(arr,low,high):
+    if low < high:
+        pi = partition(arr,low,high)
+        QuickSort(arr, low, pi - 1)
+        QuickSort(arr, pi + 1, high)
+
+
+arr1=[]
+for i in range(50000):
+    x=randint(0,1000)       #następuje zwolnienie komory losującej
+    arr1.append(x)
+arr2=arr1
+
+
+
+
+
+
+
+
+
 t_start = perf_counter_ns()
-TimSort(arr)
+QuickSort(arr1,0,len(arr1) - 1)
 t_stop = perf_counter_ns()
-print("czas sortowania, zbior losowy, Tim:",
+print("czas sortowania, zbior losowy, QuickSort:",
+                                        t_stop-t_start)
+
+t_start = perf_counter_ns()
+QuickSort(arr1,0,len(arr1) - 1)
+t_stop = perf_counter_ns()
+print("czas sortowania, zbior posortowany, QuickSort:",
                                          t_stop-t_start)
 
-
+arr1.reverse()
 t_start = perf_counter_ns()
-TimSort(arr)
+QuickSort(arr1,0,len(arr1) - 1)
+t_stop = perf_counter_ns()
+print("czas sortowania, zbior posortowany odwrotnie, Qs :",
+                                         t_stop-t_start, "\n")
+
+
+
+
+
+arr1=arr2
+t_start = perf_counter_ns()
+TimSort(arr1)
+t_stop = perf_counter_ns()
+print("czas sortowania, zbior losowy, TimSort",
+                                         t_stop-t_start)
+t_start = perf_counter_ns()
+TimSort(arr1)
 t_stop = perf_counter_ns()
 print("czas sortowania, zbior posortowany, Tim:",
                                          t_stop-t_start)
-
-
-
-arr=[27,21,14,13,10,8,7,4,3,3,3]
+arr1.reverse()
 t_start = perf_counter_ns()
-TimSort(arr)
+TimSort(arr1)
 t_stop = perf_counter_ns()
 print("czas sortowania, zbior posortowany odwrotnie, Tim:",
-                                         t_stop-t_start)
+                                         t_stop-t_start,"\n")
 
 
-arr = [7,4,3,8,10,3,3,13,14,21,27]
+
+arr1=arr2
 t_start = perf_counter_ns()
-qs(arr,0,len(arr) - 1)
-t_stop = perf_counter_ns()
-print("czas sortowania, zbior losowy, Qs:",
-                                         t_stop-t_start)
-
-t_start = perf_counter_ns()
-qs(arr,0,len(arr) - 1)
-t_stop = perf_counter_ns()
-print("czas sortowania, zbior posortowany, Qs:",
-                                         t_stop-t_start)
-
-arr=[27,21,14,13,10,8,7,4,3,3,3]
-t_start = perf_counter_ns()
-qs(arr,0,len(arr) - 1)
-t_stop = perf_counter_ns()
-print("czas sortowania, zbior posortowany odwrotnie, Qs:",
-                                         t_stop-t_start)
-
-arr = [7,4,3,8,10,3,3,13,14,21,27]
-t_start = perf_counter_ns()
-Heap_Sort(arr)
-Build_Heap(arr, 3);
+Heap_Sort(arr1)
+Build_Heap(arr1, 3)
 t_stop = perf_counter_ns()
 print("czas sortowania, zbior losowy, Heapsort:",
                                          t_stop-t_start)
-
 t_start = perf_counter_ns()
-Heap_Sort(arr)
-Build_Heap(arr, 3);
+Heap_Sort(arr1)
+Build_Heap(arr1, 3)
 t_stop = perf_counter_ns()
 print("czas sortowania, zbior posegregowany, Heapsort:",
                                          t_stop-t_start)
-
-arr=[27,21,14,13,10,8,7,4,3,3,3]
 t_start = perf_counter_ns()
-Heap_Sort(arr)
-Build_Heap(arr, 3);
+Heap_Sort(arr1)
+Build_Heap(arr1, 3)
 t_stop = perf_counter_ns()
 print("czas sortowania, zbior posegregowany odwrotnie, Heapsort:",
-                                         t_stop-t_start)
-
-
-
+                                         t_stop-t_start,"\n")
 
 
 
